ad236fd1b03f9338ea434714c97f500f28904e2c
axitdhola <201901004@daiict.ac.in>
2024-07-31 12:40:50 +0530

    Added file structure

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello async World"}
+
+@app.post("/id")
+async def printId(id: int):
+    return {"id": 'Hello ' + str(id) + ' World'}
